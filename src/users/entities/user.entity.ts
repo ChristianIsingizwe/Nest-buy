c0e315cd64773355410dd54e209e88ea299528e3
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-08-23 18:47:21 +0200

    Made some configuratios

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,5 @@
 import { Roles } from "src/utility/common/user-roles.enum";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
 
 @Entity('users')
 export class UserEntity {
@@ -9,13 +9,18 @@ export class UserEntity {
     @Column()
     name: string
 
-    @Column()
+    @Column({unique: true})
     email: string
 
-    @Column()
+    @Column({select: false})
     password: string
 
     @Column({type: 'enum', enum:Roles, array: true, default:[Roles.USER]})
     roles: Roles[]
 
+    @CreateDateColumn()
+    createdAt:Timestamp
+
+    @UpdateDateColumn()
+    updatedAt: Timestamp
 }
