165de96657e83ace83b76a2251eed3edb67de036
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2024-08-23 21:03:51 +0200

    Authorization

--- src/utility/middlewares/current-user.middleware.ts
+++ src/utility/middlewares/current-user.middleware.ts
@@ -1,10 +1,42 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { isArray } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserEntity;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(req);
-    next();
+  constructor(private readonly usersService: UsersService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (
+      !authHeader ||
+      isArray(authHeader) ||
+      !authHeader.startsWith('Bearer')
+    ) {
+      req.currentUser = null;
+      next();
+    } else {
+      const token = authHeader.split(' ')[1];
+      const { id } = <JwtPayload>(
+        verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+      );
+      const currentUser = await this.usersService.findOne(+id);
+      req.currentUser = currentUser;
+      next();
+    }
   }
+}
+
+interface JwtPayload {
+  id: string;
 }
